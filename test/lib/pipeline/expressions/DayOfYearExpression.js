ff6f02fe814172d5cd3582ecf126f7c85b0494b5
Spencer Rathbun <srathbun@riverainc.com>
2013-02-18 22:17:32 +0000

    Refs #965. Added test case for DayOfYearExpression and made minor bug fixes.
    http://source.rd.rcg.local/trac/eagle6/changeset/1272/Eagle6_SVN

---
+++ test/lib/pipeline/expressions/DayOfYearExpression.js
@@ -0,0 +1,47 @@
+var assert = require("assert"),
+	DayOfYearExpression = require("../../../../lib/pipeline/expressions/DayOfYearExpression"),
+	Expression = require("../../../../lib/pipeline/expressions/Expression");
+
+module.exports = {
+
+	"DayOfYearExpression": {
+
+		"constructor()": {
+
+			"should not throw Error when constructing without args": function testConstructor(){
+				assert.doesNotThrow(function(){
+					new DayOfYearExpression();
+				});
+			}
+
+		},
+
+		"#getOpName()": {
+
+			"should return the correct op name; $dayOfYear": function testOpName(){
+				assert.equal(new DayOfYearExpression().getOpName(), "$dayOfYear");
+			}
+
+		},
+
+		"#getFactory()": {
+
+			"should return the constructor for this class": function factoryIsConstructor(){
+				assert.strictEqual(new DayOfYearExpression().getFactory(), undefined);
+			}
+
+		},
+
+		"#evaluate()": {
+
+			"should return day of year; 49 for 2013-02-18": function testStuff(){
+				assert.strictEqual(Expression.parseOperand({$dayOfMonth:"$someDate"}).evaluate({someDate:new Date("2013-02-18")}), 49);
+			}
+
+		}
+
+	}
+
+};
+
+if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
