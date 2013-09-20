1ba98844983d78c6094d74ffcb94e069feed9529
Brennan Chesley <bchesley@riverainc.com>
2013-09-20 11:42:23 -0400

    ref #3256: Added tests, passes all (5) tests

--- test/lib/pipeline/matcher/ComparisonMatchExpression.js
+++ test/lib/pipeline/matcher/ComparisonMatchExpression.js
@@ -5,6 +5,7 @@ var assert = require("assert"),
 
 module.exports = {
 	"ComparisonMatchExpression": {
+
 		"Should properly initialize with an empty path and a number": function (){
 			var e = new ComparisonMatchExpression();
 			e._matchType = 'LT';
@@ -17,8 +18,32 @@ module.exports = {
 			assert.strictEqual(e.init('',{})['code'],'BAD_VALUE');	
 			assert.strictEqual(e.init('',undefined)['code'],'BAD_VALUE');
 			assert.strictEqual(e.init('',{})['code'],'BAD_VALUE');
-			}
+		},
+		"Should match numbers with GTE": function (){
+			var e = new ComparisonMatchExpression();
+			e._matchType = 'GTE';
+			assert.strictEqual(e.init('',5)['code'],'OK');
+			assert.ok(e.matchesSingleElement(6), "6 ≥ 5");
+			assert.ok(e.matchesSingleElement(5), "5 ≥ 5");
+			assert.ok(!e.matchesSingleElement(4), "4 ≥ 5");
+			assert.ok(!e.matchesSingleElement('foo'), "5 ≥ 'foo'");
+		},
+		"Should match with simple paths and GTE": function(){
+			var e = new ComparisonMatchExpression();
+			e._matchType = 'GTE';
+			assert.strictEqual(e.init('a', 5)['code'],'OK');
+			assert.ok(e.matches({'a':6}));
+		},
+		"Should match arrays with GTE": function (){
+			var e = new ComparisonMatchExpression();
+			e._matchType = 'GTE';
+			assert.strictEqual(e.init('a',5)['code'],'OK');
+			assert.ok(e.matches({'a':[6,10]}),'[6,10] ≥ 5');
+			assert.ok(e.matches({'a':[4,5.5]}), '[4,5.5] ≥ 5');
+			assert.ok(!e.matches({'a':[1,2]}),'[1,2] ≥ 5');
+			assert.ok(e.matches({'a':[1,10]}),'[1,10] ≥ 5');
 		}
+	}
 };
 
 if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
