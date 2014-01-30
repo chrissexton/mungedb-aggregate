a4a5e96b569e7f448dd36c891f9c52f5ca5ad28b
Chris Sexton <CSexton@RiveraInc.com>
2014-01-30 14:46:33 -0500

    Refs #5121: Add test cases for depsToProjection

--- test/lib/pipeline/documentSources/DocumentSource.js
+++ test/lib/pipeline/documentSources/DocumentSource.js
@@ -15,6 +15,27 @@ module.exports = {
 
 				assert.equal(expected, JSON.stringify(proj));
 			},
+			"should be able to convert dependencies with subfields to a projection": function(){
+				var array = {'a':1,'a.b':1},
+					expected = '{"_id":0,"a":1}',
+					proj = DocumentSource.depsToProjection(array);
+
+				assert.equal(expected, JSON.stringify(proj));
+			},
+			"should be able to convert dependencies with _id to a projection": function(){
+				var array = {"_id":1,'a':1,'b':1},
+					expected = '{"a":1,"b":1,"_id":1}',
+					proj = DocumentSource.depsToProjection(array);
+
+				assert.equal(expected, JSON.stringify(proj));
+			},
+			"should be able to convert dependencies with id and subfields to a projection": function(){
+				var array = {'_id.a':1,'b':1},
+					expected = '{"_id":1,"b":1}',
+					proj = DocumentSource.depsToProjection(array);
+
+				assert.equal(expected, JSON.stringify(proj));
+			},
 		}
 
 	}
