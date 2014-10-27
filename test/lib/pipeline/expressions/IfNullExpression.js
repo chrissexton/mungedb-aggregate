3f2676354cee2a9eccf1143635f410b6c86108d5
Tony Ennis <tennis@riverainc.com>
2014-10-27 15:24:01 -0400

    EAGLESIX- filling out the test cases, minor changes to expression classes.

--- test/lib/pipeline/expressions/IfNullExpression.js
+++ test/lib/pipeline/expressions/IfNullExpression.js
@@ -1,58 +1,52 @@
 "use strict";
 var assert = require("assert"),
-		IfNullExpression = require("../../../../lib/pipeline/expressions/IfNullExpression"),
-		Expression = require("../../../../lib/pipeline/expressions/Expression");
+	IfNullExpression = require("../../../../lib/pipeline/expressions/IfNullExpression"),
+	Expression = require("../../../../lib/pipeline/expressions/Expression");
 
 
 module.exports = {
 
-		"IfNullExpression": {
+	"IfNullExpression": {
 
-				"constructor()": {
+		"constructor()": {
 
-						"should not throw Error when constructing without args": function testConstructor() {
-								assert.doesNotThrow(function() {
-										new IfNullExpression();
-								});
-						}
+			"should not throw Error when constructing without args": function() {
+				assert.doesNotThrow(function () {
+					new IfNullExpression();
+				});
+			},
+			"should throw Error when constructing with args": function () {
+				assert.throws(function () {
+					new IfNullExpression();
+				});
+			}
+		},
 
-				},
+		"#getOpName()": {
 
-				"#getOpName()": {
+			"should return the correct op name; $ifNull": function() {
+				assert.equal(new IfNullExpression().getOpName(), "$ifNull");
+			}
 
-						"should return the correct op name; $ifNull": function testOpName() {
-								assert.equal(new IfNullExpression().getOpName(), "$ifNull");
-						}
+		},
 
-				},
+		"#evaluateInternal()": {
+			beforeEach: function () {
+				this.expr = {$ifNull:["$a", "$b"]};
+				this.parsed = Expression.parseOperand(this.expr, {});
+			},
 
-				"#evaluateInternal()": {
-
-						"should return the left hand side if the left hand side is not null or undefined": function testStuff() {
-								assert.strictEqual(Expression.parseOperand({
-										$ifNull: ["$a", "$b"]
-								}).evaluateInternal({
-										a: 1,
-										b: 2
-								}), 1);
-						},
-						"should return the right hand side if the left hand side is null or undefined": function testStuff() {
-								assert.strictEqual(Expression.parseOperand({
-										$ifNull: ["$a", "$b"]
-								}).evaluateInternal({
-										a: null,
-										b: 2
-								}), 2);
-								assert.strictEqual(Expression.parseOperand({
-										$ifNull: ["$a", "$b"]
-								}).evaluateInternal({
-										b: 2
-								}), 2);
-						}
-				}
-
+			"should return the left hand side if the left hand side is not null or undefined": function() {
+				assert.strictEqual(this.parsed.evaluateInternal({a: 1, b: 2}), 1);
+			},
+			"should return the right hand side if the left hand side is null": function() {
+				assert.strictEqual(this.parsed.evaluateInternal({a: null, b: 2}), 2);
+			},
+			"should return the right hand side if the left hand side is undefined": function() {
+				assert.strictEqual(this.parsed.evaluateInternal({b: 2}), 2);
+			}
 		}
-
+	}
 };
 
-if (!module.parent)(new(require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
+if (!module.parent)(new (require("mocha"))()).ui("exports").reporter("spec").addFile(__filename).run(process.exit);
