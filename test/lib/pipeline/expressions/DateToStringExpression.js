4c8ad78af80f5b4744b314783717b95c9e888a31
Jason Walton <JWalton@riverainc.com>
2014-10-27 10:15:19 -0400

    Added a few DateToString test case skeletons

--- test/lib/pipeline/expressions/DateToStringExpression.js
+++ test/lib/pipeline/expressions/DateToStringExpression.js
@@ -10,20 +10,32 @@ module.exports = {
 
 	"DateToStringExpression": {
 
-			"constructor()": {
+		"constructor()": {
 
-					"should throw Error when constructing without args": function testConstructor() {
-							assert.throws(function() {
-									new DateToStringExpression();
-							});
-					},
-
-					"should not throw Error when constructing with an arg": function testConstructor() {
-							assert.doesNotThrow(function() {
-									new DateToString("1/1/2014");
-							});
-					}
-
+			"should throw Error when constructing without args": function testConstructor() {
+					assert.throws(function() {
+							new DateToStringExpression();
+					});
 			},
+
+			"should not throw Error when constructing with 2 args": function testConstructor() {
+					assert.doesNotThrow(function() {
+							new DateToString("%Y%m%d", "1/1/2014");
+					});
+			}
+		},
+
+		"#getOpName()" : {
+
+			"should return the correct opName: $dateToString": function testOpName() {
+				assert.equal(new DateToStringExpression("%Y%m%d", "1/1/2014").getOpName(), "$dateToString");
+			};
+		},
+
+		"evaluateInternal1()" : {
+			"should return the date to string": function evaluateInternal1() {
+				assert.equal(new DateToStringExpression("%Y%m%d", "1/1/2014").evaluateInternal(), "2014/1/1");
+			};
 		}
 	}
+}
